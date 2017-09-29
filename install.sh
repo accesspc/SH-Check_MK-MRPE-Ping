81848b84e98ceb5667d29e45fc10c341557e2635
Robertas Reiciunas <Robertas.Reiciunas@precedent.com>
2017-09-29 11:29:58 +0100

    Setting up

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x 
+#set -x 
 
 if [ ! -x /usr/lib/nagios/plugins/check_ping ] ; then
 	echo "nagios-plugins is not installed"
@@ -20,6 +20,8 @@ plugin_mcp=$(pwd)/plugin/mrpe_check_ping
 
 if [ -x ${cron_mcp} ] ; then
 	echo "Cron '${cron_mcp}' already exists. Overwritting..."
+else
+	echo "New cron job: ${cron_mcp}"
 fi
 
 
@@ -28,6 +30,7 @@ echo "#
 # cron.d/mrpe_check_ping -- schedules threaded check_mk check_ping every minute
 # 
 * *	* * *	root	if [ -x ${bin_cpt} ]; then ${bin_cpt}; fi" > ${cron_mcp}
+cat ${cron_mcp}
 
 install -m 0755 plugin/mrpe_check_ping /usr/lib/check_mk_agent/plugins/mrpe_check_ping
 
