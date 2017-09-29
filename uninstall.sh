13b86b6360f8641f4639be3e80517f8fefd49fe5
Robertas Reiciunas <Robertas.Reiciunas@precedent.com>
2017-09-29 14:10:00 +0100

    v1.2

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,4 @@ rm -vf /etc/cron.d/mrpe_check_ping
 rm -vf /etc/logrotate.d/mrpe_check_ping
 rm -vf /usr/lib/check_mk_agent/plugins/mrpe_check_ping
 rm -vf /var/log/mrpe_check_ping.log*
+rm -rvf /tmp/mrpe
