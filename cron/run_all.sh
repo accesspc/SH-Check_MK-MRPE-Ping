5fa3368033bfb9636b4ba328a337fc9cc3fd683d
Robertas Reiciunas <Robertas.Reiciunas@precedent.com>
2017-09-29 10:58:06 +0100

    Setting up

--- cron/run_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for l in `cat /etc/check_mk/mrpe.cfg | grep '/usr/lib/nagios/plugins/check_ping'` ; do
-	
-
-done
--- install.sh
+++ install.sh
@@ -5,32 +5,20 @@ if [ ! -x /usr/lib/nagios/plugins/check_ping ] ; then
 	exit 1
 fi
 
-
-
 echo "Installing"
 cwd=`pwd`
 
-bin_check_ping=$cwd/bin/check_ping
+if [ -x /etc/cron.d/mrpe_check_ping ] ; then
+	echo "Cron '/etc/cron.d/mrpe_check_ping' already exists"
+	echo "Overwritting..."
+fi
 
-install -m 0777 $cwd/bin/mrpe_check_ping /usr/lib/check_mk_agent/plugins/mrpe_check_ping
+mcp=$(pwd)/bin/mrpe-check_ping
 
+echo "# " > /etc/cron.d/mrpe_check_ping
+echo "# cron.d/mrpe_check_ping -- schedules threaded check_mk check_ping every minute" >> /etc/cron.d/mrpe_check_ping
+echo "# " >> /etc/cron.d/mrpe_check_ping
+echo "* *	* * *	root	if [ -x ${mcp} ]; then ${mcp}; fi"
 
-
-exit 0
-
-apt-get purge iperf3 libiperf0
-wget https://iperf.fr/download/ubuntu/libiperf0_3.1.3-1_amd64.deb
-wget https://iperf.fr/download/ubuntu/iperf3_3.1.3-1_amd64.deb
-dpkg -i libiperf0_3.1.3-1_amd64.deb iperf3_3.1.3-1_amd64.deb
-rm -vf libiperf0_3.1.3-1_amd64.deb iperf3_3.1.3-1_amd64.deb
-
-echo "Press enter to install as a service or Ctrl+C to stop here"
-
-read
-
-#cp systemd/iperf3.service /etc/systemd/system/iperf3.service
-wget -O /etc/systemd/system/iperf3.service http://rrt-wtg.precedenthost.local:3000/TS/sw/raw/master/iperf3/systemd/iperf3.service
-systemctl daemon-reload
-systemctl enable iperf3.service
-systemctl start iperf3.service
+echo "Finished"
 
