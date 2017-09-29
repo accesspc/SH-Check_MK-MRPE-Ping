3d65ec7de4a2a13c7b30e38157ce87156dbcbc76
Robertas.Reiciunas <Robertas.Reiciunas@prec-cln023.Precedent.local>
2017-09-29 16:15:36 +0100

    v1.1.1

--- install.sh
+++ install.sh
@@ -35,6 +35,7 @@ cat ${cron_mcp}
 install -v -m 0644 logrotate.d/mrpe_check_ping /etc/logrotate.d/mrpe_check_ping
 install -v -m 0755 plugin/mrpe_check_ping /usr/lib/check_mk_agent/plugins/mrpe_check_ping
 
+echo
 echo "Finished"
 echo
 echo "Give at least 2 minutes before discovering in Check_MK"
