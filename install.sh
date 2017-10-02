5bf77a1307be0219478e62b9369326e888da283a
Robertas.Reiciunas <Robertas.Reiciunas@prec-cln023.Precedent.local>
2017-10-02 09:58:20 +0100

    v1.1.2

--- install.sh
+++ install.sh
@@ -14,23 +14,12 @@ fi
 echo "Installing"
 cwd=`pwd`
 
-cron_mcp=/etc/cron.d/mrpe_check_ping
 bin_cpt=$(pwd)/bin/check_ping_threaded
 plugin_mcp=$(pwd)/plugin/mrpe_check_ping
 
-if [ -x ${cron_mcp} ] ; then
-	echo "Cron '${cron_mcp}' already exists. Overwritting..."
-else
-	echo "New cron job: ${cron_mcp}"
-fi
 
-
-
-echo "# 
-# cron.d/mrpe_check_ping -- schedules threaded check_mk check_ping every minute
-# 
-* *	* * *	root	if [ -x ${bin_cpt} ]; then ${bin_cpt}; fi" > ${cron_mcp} >> /var/log/mrpe_check_ping.log
-cat ${cron_mcp}
+install -v -m 0644 cron.d/mrpe_check_ping /etc/cron.d/mrpe_check_ping
+sed -s -i "s|BIN_CPT|${bin_cpt}|g" /etc/cron.d/mrpe_check_ping
 
 install -v -m 0644 logrotate.d/mrpe_check_ping /etc/logrotate.d/mrpe_check_ping
 install -v -m 0755 plugin/mrpe_check_ping /usr/lib/check_mk_agent/plugins/mrpe_check_ping
